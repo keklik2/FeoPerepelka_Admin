ff8b5630a2e171c9d9a38d47a69540eda4293e92
keklik2 <63215768+keklik2@users.noreply.github.com>
2022-06-06 18:50:57 +0300

    framework test

--- app/src/main/java/com/demo/feoperepelkaadmin/MainActivity.kt
+++ app/src/main/java/com/demo/feoperepelkaadmin/MainActivity.kt
@@ -2,10 +2,22 @@ package com.demo.feoperepelkaadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.parse.ParseObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv = findViewById<TextView>(R.id.tv)
+
+        val firstObject = ParseObject("FirstClass")
+        firstObject.put("message","Hey ! First message from android. Parse is now connected")
+        firstObject.saveInBackground {
+            if (it != null){ it.localizedMessage?.let { msg -> tv.text = "error: $msg" }
+            } else { tv.text = "success" }
+        }
     }
 }
